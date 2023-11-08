7b330d69dcd39f64a1862dd52a82fb11c834756a
bkstud <bkstud@em.com>
2023-11-08 23:29:13 +0100

    testing lazy setup looks

--- init.lua
+++ init.lua
@@ -2,39 +2,65 @@
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
--- enable 24-but rgb color in the TUI
-vim.opt.termguicolors = true
 
--- packer plugins configuration
--- Install packer if it's not already installed
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local packer_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = true
-    vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Install lazy nvim if it"s not already installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- Automatically set up your configuration after cloning packer.nvim
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+require("lazy").setup({
+  -- Theme
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").load()
+    end
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = "onedark",
+        component_separators = "|",
+        section_separators = "",
+      },
+      sections = {
+        lualine_b = {'filename'},
+        lualine_c = {'branch'},
+        lualine_x = {},
+        lualine_y = {'diagnostics'},
+      },
+    },
+  },
+
+},{})
 
 -- vim opts
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
 
 -- tabs & indentation
-vim.opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
-vim.opt.softtabstop = 4 -- 4 spaces for tabs (prettier default)
-vim.opt.shiftwidth = 4 -- 4 spaces for indent width
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.autoindent = true -- copy indent from current line when starting new one
 vim.opt.expandtab = true -- expand tabs
 vim.opt.smartindent = true -- indents smart
-
+-- enable 24-but rgb color in the TUI
+vim.opt.termguicolors = true
 -- line wrapping
 vim.opt.wrap = false -- do not use linewrap by default
 vim.opt.linebreak = false -- do not break by words at the end of the line
@@ -48,10 +74,10 @@ vim.opt.clipboard = unnamedplus
 
 -- keymaps
 -- nohl on double esc
-vim.keymap.set({ 'n','v' },'<Esc><Esc>', ':let @/ = ""<CR>', { silent = true })
+vim.keymap.set({ "n","v" },"<Esc><Esc>", ":let @/ = ''<CR>", { silent = true })
 -- use ; as : very convenient as not need to hold shift
-vim.keymap.set({ 'n','v','o' }, ';', ':')
+vim.keymap.set({ "n","v","o" }, ";", ":")
 -- instead the repeat f, t command is set to ,
-vim.keymap.set({'n','v','o'}, ',', ';')
+vim.keymap.set({"n","v","o"}, ",", ";")
 -- repeat back f, t command is set to leader + ,
-vim.keymap.set({'n','v','o'}, '<Leader>,', ',')
+vim.keymap.set({"n","v","o"}, "<Leader>,", ",")
