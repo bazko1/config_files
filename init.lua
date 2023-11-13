99f2a3ca8ed99ac7385b7df855f9fbcf5b67f968
bkstud <bkstud@em.com>
2023-11-13 19:23:33 +0100

    goimport +fmt on save

--- init.lua
+++ init.lua
@@ -318,7 +318,7 @@ local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').gofmt()
+   require('go.format').goimport() -- goimport + gofmt
   end,
   group = format_sync_grp,
 })
