7e1164c31c9c0823a2e82da17ff096a5e72d5d27
bkstud <bkstud@em.com>
2024-02-03 15:56:32 +0100

    create initial install script

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-force=false
+# User definable variables
+INSTALL_DIR="${INSTALL_DIR:-$HOME}"
+FORCE="${FORCE:-false}"
 
-createHomeLink ()
+echo "Installing configuration to $INSTALL_DIR"
+
+createLink ()
 {
   args="-s"
-  $force && args+=" -f"
-  ln "$args" "${SCRIPT_DIR}/$1" "${HOME}/$2"
+  $FORCE && args+="f"
+  ln "$args" "${SCRIPT_DIR}/$1" "${INSTALL_DIR}/$2"
 }
 
 createLink ".gitconfig" ".gitconfig"
@@ -16,8 +20,10 @@ createLink ".inputrc" ".inputrc"
 createLink ".tmux.conf" ".tmux.conf"
 createLink ".bash_aliases" ".bash_aliases"
 
-mkdir -p "${HOME}/.config/nvim" 
+mkdir -p "${INSTALL_DIR}/.config/nvim"
 createLink "init.lua" ".config/nvim"
 
-bashAlias="[ -f ~/.aliasrc ] && . ./.aliasrc"
-
+if ! [ -f "/etc/wsl.conf" ]; then
+  mkdir -p "${INSTALL_DIR}/.config/alacritty"
+  createLink "alacritty-linux.yml" ".config/alacritty/alacritty.yml"
+fi
