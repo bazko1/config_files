54a1e73fc72a0144627bc619899d207dd4d49dac
Kot, Bazyli <bazylix.kot@intel.com>
2024-02-09 11:41:58 +0100

    position tree right

--- init.lua
+++ init.lua
@@ -268,7 +268,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [W]ord' })
 vim.keymap.set('n', '<leader>rs', require('telescope.builtin').resume, { desc = '[R]esume [S]earch for previous picker' })
 -- neotree maps
-vim.keymap.set('n', '<leader>t', function() require('neo-tree.command').execute{ toggle=true } end, { desc = '[T]oggle NeoTree' })
+vim.keymap.set('n', '<leader>t', function() require('neo-tree.command').execute{ toggle=true, position='right'} end, { desc = '[T]oggle NeoTree' })
 
 -- [[ Configure Treesitter ]]
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
