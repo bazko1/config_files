275ec2a7882261899f38290f86d27c9bac6e434f
bazko1 <bazyli.kot.dev@gmail.com>
2024-03-24 16:18:43 +0100

    add telescope registers list bind

--- init.lua
+++ init.lua
@@ -282,6 +282,7 @@ end, { desc = 'Search [B]uffers' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch [G]rep' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [W]ord' })
 vim.keymap.set('n', '<leader>rs', require('telescope.builtin').resume, { desc = '[R]esume [S]earch for previous picker' })
+vim.keymap.set('n', '<leader>re', require('telescope.builtin').registers, { desc = '[R][e]gister' })
 -- neotree maps
 vim.keymap.set('n', '<leader>t', function() require('neo-tree.command').execute{ toggle=true, position='right'} end, { desc = '[T]oggle NeoTree' })
 
