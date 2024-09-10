4447d262091469a50c986c9e352146ce3cfa2b30
bazko1 <bazyli.kot.dev@gmail.com>
2024-09-10 12:46:28 +0200

    add vue installation

--- init.lua
+++ init.lua
@@ -325,7 +325,8 @@ vim.keymap.set('n', '<leader>t', function() require('neo-tree.command').execute 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'yaml' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx',
+      'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'yaml', 'vue' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
