32a4fef84319241edd9bcfec42936ff855d8fa4c
bazko1 <bazyli.kot.dev@gmail.com>
2024-09-18 15:25:08 +0200

    add convenient command for accessing python interpreter

--- init.lua
+++ init.lua
@@ -520,6 +520,7 @@ require("go").setup({
 -- custom commands
 vim.api.nvim_create_user_command('TrimWhiteSpace', "%s/\\s\\+$//e", {})
 vim.api.nvim_create_user_command('FormatJSON', ":%!jq .", {})
+vim.api.nvim_create_user_command('Python3', ":vs | term python3", {})
 
 -- call live_grep over specific filetypes
 vim.api.nvim_create_user_command('LiveGrepType', function(opts)
