4f4735f63cdd4f104ffd3f2a66979098e80c6a5c
bazko1 <bazyli.kot.dev@gmail.com>
2025-04-17 12:51:26 +0200

    change font size for icons, fish hybrid key binds to enable some shortcuts

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-     fish_vi_key_bindings
+     # fish_vi_key_bindings
+     fish_hybrid_key_bindings
 end
 
 source ~/.bash/aliases.sh
