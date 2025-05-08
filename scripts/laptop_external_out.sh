7bb8d4214d9612753afabf5423c190286833bb33
bazko1 <bazyli.kot.dev@gmail.com>
2025-05-08 14:03:55 +0200

    rename display

--- scripts/laptop_external_out.sh
+++ scripts/laptop_external_out.sh
@@ -4,7 +4,7 @@
 displays=$(xrandr | grep " connected")
 num_display=$(echo "$displays" | wc -l)
 if  [ "$num_display" -gt 1 ]; then
-  xrandr --auto --output eDP-1-1 --off
+  xrandr --auto --output eDP-1 --off
 else
   xrandr --auto
 fi
