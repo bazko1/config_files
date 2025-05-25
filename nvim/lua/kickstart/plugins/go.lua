e3d7c2b4eb301828c7c5ad11996b1e1081b2eda5
bazko1 <bazyli.kot.dev@gmail.com>
2025-05-25 22:29:02 +0200

    add optional dependency

--- nvim/lua/kickstart/plugins/go.lua
+++ nvim/lua/kickstart/plugins/go.lua
@@ -5,6 +5,7 @@ return {
       'ray-x/guihua.lua',
       'neovim/nvim-lspconfig',
       'nvim-treesitter/nvim-treesitter',
+      'theHamsta/nvim-dap-virtual-text',
     },
     opts = {
       luasnip = true,
