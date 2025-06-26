c9d74c48d729bf2ef9d67ef8f5ef042498662637
bazko1 <bazyli.kot.dev@gmail.com>
2025-06-26 12:50:14 +0200

    configure cd on quit

--- bash/functions.sh
+++ bash/functions.sh
@@ -5,3 +5,16 @@ gfl() { git log --pretty=oneline --abbrev-commit | fzf --preview-window down:70%
 docker_rm_unnamed() { docker image rm "$(docker image ls | grep '^<none>' | awk '{print $3}')"; }
 sshf() { fzf  --preview 'echo {1}@{2}' --bind 'enter:become(ssh {1}@{2})' < ~/.scripts/ssh_connections.txt; }
 __git_branch__() { git rev-parse --is-inside-work-tree 1>/dev/null 2>/dev/null && echo "(""$(git branch --show-current)"") "; }
+n() {
+  if [ -n "$NNNLVL" ] && [ "$NNNLVL" -ge 1 ]; then
+    echo "nnn is already running"
+    return
+  fi
+
+  export NNN_TMPFILE="$HOME/.config/nnn/.lastd"
+  nnn -adeHo "$@"
+  if [ -f "$NNN_TMPFILE" ]; then
+    . "$NNN_TMPFILE"
+    rm -f "$NNN_TMPFILE" > /dev/null
+  fi
+}
