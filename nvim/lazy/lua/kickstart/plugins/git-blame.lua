1f3d927eda22864cc811d876f131b92fdd08b9fb
bazko1 <bazyli.kot.dev@gmail.com>
2025-12-08 10:50:26 +0100

    call on init

--- nvim/lazy/lua/kickstart/plugins/git-blame.lua
+++ nvim/lazy/lua/kickstart/plugins/git-blame.lua
@@ -9,24 +9,29 @@ return {
     config = function(_, opts)
       require('gitblame').setup(opts)
 
-      vim.keymap.set('n', '<leader>gb', ':GitBlameEnable<CR>', { desc = '[G]it [B]lame', silent = true })
+      vim.keymap.set('n', '<leader>gb', ':GitBlameToggle<CR>', { desc = '[G]it [B]lame', silent = true })
 
-      -- change remote name to upstream on certain repos
       local upstream_repos = {
         'minio',
         'git-blame.nvim',
       }
+      -- change remote name to upstream on certain repos
       local root_repos_path = vim.fn.expand '~' .. '/gitworkspace'
-      vim.api.nvim_create_autocmd({ 'DirChanged' }, {
-        callback = function()
-          for _, repo in ipairs(upstream_repos) do
-            local repo_path = vim.fs.abspath(vim.fs.joinpath(root_repos_path, repo))
-            if vim.startswith(vim.fn.getcwd(), repo_path) then
-              vim.g.gitblame_remote_name = 'upstream'
-            end
+      local set_remote_name = function()
+        for _, repo in ipairs(upstream_repos) do
+          local repo_path = vim.fs.abspath(vim.fs.joinpath(root_repos_path, repo))
+          if vim.startswith(vim.fn.getcwd(), repo_path) then
+            vim.g.gitblame_remote_name = 'upstream'
           end
-        end,
+        end
+      end
+
+      vim.api.nvim_create_autocmd({ 'DirChanged' }, {
+        callback = set_remote_name,
       })
+
+      -- call once on init
+      set_remote_name()
     end,
   },
 }
